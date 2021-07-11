63c2e34451a28ad0b4abfcf7b9468724540eb1c8
Durgesh Singh <45793814+DurgeshSingh043@users.noreply.github.com>
2021-07-11 08:41:40 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+const YELP_KEY =
+  'EiM7V1DDoVrAa5BvZbnCEhJUNvpWFVjjEcet4BfotoXl8l_aiKrPx_NfDFRWlBef2Imr5FKmnWMqWhs017ac95dSFDI_otKeeEuvsfkobVQEieRaiLOOSEJECjR2YHYx';
+
+const YELP_SEARCH_PARKING_API_URL =
+  'https://api.yelp.com/v3/businesses/search?term=parking&location=';
+
+module.exports = {
+  YELP_KEY: YELP_KEY,
+  YELP_SEARCH_PARKING_API_URL: YELP_SEARCH_PARKING_API_URL,
+};
---
+++ src/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const { YELP_SEARCH_PARKING_API_URL, YELP_KEY } = require('./constants');
+
+app.use(cors());
+app.options('*', cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//create a server object:
+app.get('/', function (req, res) {
+  res.write('Hello from Durgesh Singh!...');
+  res.end(); //end the response
+});
+
+app.get('/getparkingbylocation', function (req, res) {
+  const { location } = req.query || {};
+  console.log('location ', location);
+  if (location) {
+    axios
+      .get(`${YELP_SEARCH_PARKING_API_URL}${location}`, {
+        headers: {
+          Authorization: `Bearer ${YELP_KEY}`,
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
+      .then((response) => {
+        res.status(200).send(response.data);
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .send(`Something went wrong due to ${err && err.message}`);
+      });
+  } else {
+    res.status(500).send('Please enter valid address.');
+  }
+});
+
+app.listen(8080, function () {
+  console.log('server running on 8080');
+}); //the server object listens on port 8080
